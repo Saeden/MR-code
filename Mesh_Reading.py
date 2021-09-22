4921311130d3af4440762456e9137c3163388f13
Dannydigr <dani.di.grandi#gmail.com>
2021-09-22 16:13:54 +0200

    Organized code

--- Mesh_Reading.py
+++ Mesh_Reading.py
@@ -12,46 +12,58 @@ import trimesh
 import pyrender
 
 
-# attach to logger so trimesh messages will be printed to console
-trimesh.util.attach_to_log()
+def load_mesh(filepath):
 
-"""
-FILE PATH:
-"""
-file = "./benchmark/db/0//m99/m99.off"
+    # attach to logger so trimesh messages will be printed to console
+    trimesh.util.attach_to_log()
 
-"""
-LOAD THE MODEL:
-by default, trimesh will do a light processing, which will
-remove any NaN values and merge vertices that share position
-if you want to not do this on load, you can pass `process=False`
-"""
-#mesh = trimesh.load(file)
+    """
+    LOAD THE MODEL:
+    by default, trimesh will do a light processing, which will
+    remove any NaN values and merge vertices that share position
+    if you want to not do this on load, you can pass `process=False`
+    """
+    raw_mesh = trimesh.load(filepath)
 
-"""
-Preview mesh in an OpenGL window (need pyglet and scipy to be installed):
-"""
-#mesh.show(smooth=False, line_settings={'point_size': 20, 'line_width': 1})
+    """
+    OLD ATTEMPT
+    Preview mesh in an OpenGL window (need pyglet and scipy to be installed):
+    """
+    # raw_mesh.show(smooth=False, line_settings={'point_size': 20, 'line_width': 1})
+
+    """
+    NEW ATTEMPT:
+    Use trimesh only to load the file and pyrender to view it
+
+    To run pyrender on MAC:
+    edit PyOpenGL file in venv/lib/python3.7/site-packages/OpenGL/platform/ctypesloader.py changing line
+
+        fullName = util.find_library( name )
+    to
+
+        fullName = '/System/Library/Frameworks/OpenGL.framework/OpenGL'
+
+    """
+
+    mesh = pyrender.Mesh.from_trimesh(raw_mesh)
+
+    return mesh
 
 
-"""
-NEW ATTEMPT:
-Use trimesh only to load the file and pyrender to view it
+def view_scene(mesh):
+    """
+    Function used to view a mesh
+    It constructs an object scene and open the passed mesh
+    """
+    scene = pyrender.Scene()
+    scene.add(mesh)
+    pyrender.Viewer(scene, use_raymond_lighting=True)
 
-To run pyrender on MAC:
-edit PyOpenGL file in venv/lib/python3.7/site-packages/OpenGL/platform/ctypesloader.py changing line
 
-    fullName = util.find_library( name )
-to
 
-    fullName = '/System/Library/Frameworks/OpenGL.framework/OpenGL'
-    
-"""
-fuze_trimesh = trimesh.load(file)
-mesh = pyrender.Mesh.from_trimesh(fuze_trimesh)
-scene = pyrender.Scene()
-scene.add(mesh)
-pyrender.Viewer(scene, use_raymond_lighting=True)
+
+
+
 
 
 
---
+++ main.py
@@ -0,0 +1,14 @@
+from Mesh_Reading import *
+
+def main():
+    print("Multimedia Retrieval application")
+
+    filepath = "./benchmark/db/0//m99/m99.off"
+
+    mesh = load_mesh(filepath)
+
+    view_scene(mesh)
+
+
+if __name__ == "__main__":
+    main()
