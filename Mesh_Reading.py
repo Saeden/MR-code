6d77e754dc417e092121df1f9ea3019731e0343a
Dannydigr <dani.di.grandi#gmail.com>
2021-09-23 16:59:32 +0200

    Completed step 1

--- Mesh_Reading.py
+++ Mesh_Reading.py
@@ -3,68 +3,57 @@ import pyrender
 import open3d as o3d
 import os
 
+def casa():
+    """
+
+    :return:
+    """
 
 def load_mesh(filepath):
     """
-    Reads the mesh file located at the specified file path,
+    Reads the mesh file located at the specified filepath,
     and returns the mesh as open3d object.
-    Args:
-        file_path (str): The file path of the mesh, to be read.
-    Returns:
-        TriangleMesh: The mesh as an open3d object.
+    Furthermore, it converts all .ply files to .off files, by exporting any .ply file
+    with trimesh as a temp .off file, and then it loads the .off file.
+    Then, it cancels the temp .off file.
+    :param filepath: the filepath of the .ply or .off file containing the mesh.
+    :return: a TriangleMesh open3d object
     """
-    off = False
-    if filepath.endswith('.off'):
-        mesh = trimesh.load_mesh(filepath)
-        trimesh.exchange.export.export_mesh(mesh, './temp.ply', 'ply')
-        filepath = './temp.ply'
-        off = True
+
+    ply = False
     if filepath.endswith('.ply'):
+        mesh = trimesh.load_mesh(filepath)
+        trimesh.exchange.export.export_mesh(mesh, './temp.off', 'off')
+        filepath = './temp.off'
+        ply = True
+    if filepath.endswith('.off'):
         mesh = o3d.io.read_triangle_mesh(filepath)
     else:
-        raise ValueError('Input file must be either .OFF or .PLY format')
-    if off:
-        os.remove('./temp.ply')
+        raise ValueError('Input file must be either .off or .ply format')
+    if ply:
+        os.remove('./temp.off')
+
+    mesh.compute_vertex_normals()  # compute the light of the mesh
+
     return mesh
 
 
-def view_mesh(mesh, draw_coordinate_frame=False):
+def view_mesh(mesh, draw_coordinate_frame=False, show_wireframe=False):
     """
     Function used to view a mesh.
-    It constructs an object scene and open the passed mesh.
-    :param mesh: the mesh object that has to be displayed
-    :param pyrender_mode: True if open with pyrender, False if open with trimesh
-    :param show_3Daabb: True if 3D axis-aligned bounding box has to be showed, False otherwise
+    :param mesh: the mesh open3d object that has to be displayed.
+    :param draw_coordinate_frame: True if the 3-axis (x, y, z) has to be displayed, False otherwise.
     """
     shape = [mesh]
 
-    # The x, y, z axis will be rendered as red, green, and blue arrows
-    #  respectively.
+    # The (x, y, z) axis will be rendered as x-red, y-green, and z-blue arrows
     if draw_coordinate_frame:
-        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-            origin=[0, 0, 0])
+        mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(origin=[0, 0, 0])
         shape.append(mesh_frame)
 
-    o3d.visualization.draw_geometries(shape, mesh_show_wireframe=True)
 
-    """
-    if pyrender_mode:
+    o3d.visualization.draw_geometries(shape, mesh_show_wireframe=show_wireframe)
 
-        scene = pyrender.Scene()
-        scene.add(mesh)
-        pyrender.Viewer(scene, use_raymond_lighting=True)
-
-    else:
-        
-        #OLD ATTEMPT
-        #Preview mesh in an OpenGL window using trimesh (need pyglet and scipy to be installed):
-        
-        if show_3Daabb:
-            (mesh + mesh.bounding_box).show(smooth=False, line_settings={'point_size': 20, 'line_width': 1}, flags="wireframe")
-        else:
-            mesh.show(smooth=False, line_settings={'point_size': 20, 'line_width': 1}, flags="wireframe")
-
-    """
 
 
 
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ To make the application work, install:
 trimesh
 pyglet
 scipy
-pyrender
 open3d
 
 """
@@ -16,15 +15,13 @@ def main():
     print("Multimedia Retrieval application")
 
     filepath = "./benchmark/db/0//m99/m99.off"
-    # filepath = "/Users/danieledigrandi/Desktop/bone.ply"
+    filepath_ply = "/Users/danieledigrandi/Desktop/bone.ply"
 
     mesh = load_mesh(filepath)
 
-    view_mesh(mesh, draw_coordinate_frame=True)
+    view_mesh(mesh, draw_coordinate_frame=False, show_wireframe=True)
 
     # show_basic_statistics(mesh)
-
-
 
 
 if __name__ == "__main__":
